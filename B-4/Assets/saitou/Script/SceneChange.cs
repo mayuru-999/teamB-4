c9d2c7b1c9390f0a39a5625921816dd8236107c7
so-ya <>
2026-05-09 18:01:25 +0900

    点滅

---
+++ B-4/Assets/saitou/Script/SceneChange.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    void Update()
+    {
+        // 左クリックを検知
+        if (Input.GetMouseButtonDown(0))
+        {
+            // シーン切り替え
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
