00444af0c9a42aa77f26f40cb602f4160fe8622d
so-ya <>
2026-06-25 18:02:36 +0900

    修正中

--- B-4/Assets/saitou/Script/ButtunTitle.cs
+++ B-4/Assets/saitou/Script/ButtunTitle.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class scene3 : MonoBehaviour
 {
+    
+
+    private void Start()
+    {
+       
+    }
+    
 
     float count = 0.0f;
-    public float delay = 10.0f;
-    bool log = false;
+    public float delay = 600.0f;
+    public bool AutoChange = false;
+    public float AutoChangeCount = 1000.0f;
 
     private void Update()
     {
-        count += 0.1f;
+        count += 1.0f / Time.deltaTime; ;
 
         if (count >= delay)
         {
             Debug.Log("count >= delay");
         }
+
+        if (count >= AutoChangeCount)
+        {
+            Debug.Log("count >= AutoChangeCount");
+            if (AutoChange)
+            {
+                SceneManager.LoadScene("TitleScene");
+            }
+            
+        }
+
     }
 
 
