58cb8a609e64b43aea55dc7e478307a7b7885fe9
so-ya <>
2026-06-29 16:18:30 +0900

    resetタイミング変更

--- B-4/Assets/saitou/Script/SceneChange.cs
+++ B-4/Assets/saitou/Script/SceneChange.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class SceneChange : MonoBehaviour
 {
+
+    static bool isClick = false;
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !isClick)
         {
+            isClick = true;
             FadeManager.Instance.FadeToScene("souma.sence");
         }
     }
