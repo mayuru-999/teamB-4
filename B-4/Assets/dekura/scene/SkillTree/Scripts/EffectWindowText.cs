7b419ca0fb01e3b734aea349c4393787d37d2e26
DESKTOP-MDPR1BV\mayuru <mariru10000>
2026-06-30 18:33:59 +0900

    途中保存

---
+++ B-4/Assets/dekura/scene/SkillTree/Scripts/EffectWindowText.cs
@@ -0,0 +1,51 @@
+using TMPro;
+using UnityEngine;
+
+public class EffectWindowText : MonoBehaviour
+{
+    private EffectWindowText[] effectWindowText;
+
+    [SerializeField] private SkillEffect.Type type;
+    private TextMeshProUGUI text;
+
+    void Start()
+    {
+        text = gameObject.GetComponent<TextMeshProUGUI>();
+        UpdateUi();
+    }
+
+    public void OnClick()
+    {
+        effectWindowText = FindObjectsByType<EffectWindowText>(FindObjectsSortMode.None);
+
+        foreach (EffectWindowText EffectWindow in effectWindowText)
+            EffectWindow.UpdateUi();
+    }
+
+    public void UpdateUi()
+    {
+        float effect = SkillManage.Instance.getEffect(type);
+        if (text == null) return;
+
+        switch (type)
+        {
+            case SkillEffect.Type.Attack:
+                effect += SkillManage.Instance.baseAttack;
+                break;
+
+            case SkillEffect.Type.Speed:
+                text.text = $"-{effect}%";
+                return;
+
+            case SkillEffect.Type.Range:
+                text.text = $"+{effect}%";
+                return;
+
+            case SkillEffect.Type.PlaneVolume:
+                effect += SkillManage.Instance.basePlaneVol;
+                break;
+        }
+
+        text.text = $"{effect}";
+    }
+}
